b044bf4b8c833e454c0cab04f85d0edd80723f57
Mecopi <mecopilive@gmail.com>
2023-08-16 06:42:44 +0200

    feat: update UI

--- DofusPacketManager/Form1.cs
+++ DofusPacketManager/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using DofusPacketManager.Networking;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DofusPacketManager
@@ -26,7 +19,8 @@ namespace DofusPacketManager
 
         private void Instance_OnPacketReceived(object sender, EventArgs e)
         {
-            MessageBox.Show("Oui");
+            this.Invoke(new MethodInvoker(() => richTextBox1.Text += "Packet recieved !\n"));
         }
+
     }
 }
