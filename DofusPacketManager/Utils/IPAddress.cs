bbf2af44963b2d6ace292c07bbaa2fa6c516bbcd
Mecopi <mecopilive@gmail.com>
2023-08-16 06:41:25 +0200

    feat: add way to get IPV4 host address

---
+++ DofusPacketManager/Utils/IPAddress.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DofusPacketManager.Utils
+{
+    public static class IPAddressUtils
+    {
+        public static IPAddress GetHostV4Address() => Dns.GetHostAddresses(Dns.GetHostName()).ToList().Find((IPAddress ipAddress) => ipAddress.AddressFamily == AddressFamily.InterNetwork);
+    }
+}
