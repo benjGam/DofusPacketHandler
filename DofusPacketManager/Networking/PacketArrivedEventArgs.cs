487410e28746ba6de70c2270adb57d7059202071
Mecopi <mecopilive@gmail.com>
2023-08-16 06:40:59 +0200

    feat: add event arg for rellying packets

---
+++ DofusPacketManager/Networking/PacketArrivedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PacketDotNet;
+
+namespace DofusPacketManager.Networking
+{
+    public class PacketArrivedEventArgs : EventArgs
+    {
+        private TcpPacket _packet;
+
+        public PacketArrivedEventArgs(TcpPacket recievedPacket)
+        {
+            _packet = recievedPacket;
+        }
+
+        public TcpPacket RecievedPacket => _packet;
+    }
+}
