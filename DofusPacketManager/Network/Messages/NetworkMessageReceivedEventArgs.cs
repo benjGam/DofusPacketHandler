344a9f060f00bbdefd8ce956a14544fc29883933
Mecopi <mecopilive@gmail.com>
2023-08-17 08:24:22 +0200

    feat: add event args for message recieved

---
+++ DofusPacketManager/Network/Messages/NetworkMessageReceivedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using DofusPacketManager.Networking.Messages;
+using System;
+
+namespace DofusPacketManager.Network.Messages
+{
+    public class NetworkMessageReceivedEventArgs : EventArgs
+    {
+        private readonly NetworkMessage _recievedMessage;
+        public NetworkMessageReceivedEventArgs(NetworkMessage recievedMessage) => _recievedMessage = recievedMessage;
+        public NetworkMessage RecievedMessage => _recievedMessage;
+    }
+}
