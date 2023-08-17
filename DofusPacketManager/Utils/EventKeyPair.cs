6956a25125ea76cd3de3b0b439d754981df701a3
Mecopi <mecopilive@gmail.com>
2023-08-17 12:02:48 +0200

    feat: add way to bind specific handler on specific type on specific event

---
+++ DofusPacketManager/Utils/EventKeyPair.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DofusPacketManager.Utils
+{
+    public class EventKeyPair
+    {
+        private Dictionary<string, EventHandler> _keyPair = new Dictionary<string, EventHandler>();
+        public EventKeyPair(string eventName, EventHandler Handler)
+        {
+            AddKeyPair(eventName, Handler);
+        }
+        public void AddKeyPair(string eventName, EventHandler handler)
+        {
+            _keyPair[eventName] = handler;
+        }
+        public bool RemoveKeyPair(string eventName) 
+        { 
+            if(_keyPair.ContainsKey(eventName))
+            {
+                _keyPair.Remove(eventName);
+                return true;
+            }
+            return false;
+        }
+
+        public EventHandler GetKeyPair(string eventName)
+        {
+            if (_keyPair.ContainsKey(eventName)) return _keyPair[eventName];
+            return null;
+        }
+
+    }
+}
