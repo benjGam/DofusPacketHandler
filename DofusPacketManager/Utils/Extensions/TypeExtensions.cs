9c324ec630dbe7f1f93b5c0267991eb5b5028b29
Mecopi <mecopilive@gmail.com>
2023-08-17 07:29:06 +0200

    feat: add way to get the main class from a class

---
+++ DofusPacketManager/Utils/Extensions/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DofusPacketManager.Utils.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static Type GetOldestParent(this Type toCheck) {
+            Type toReturn = toCheck;
+            while (true)
+            {
+                if (toReturn.BaseType == null || toReturn.BaseType == typeof(object)) return toReturn;
+                toReturn = toReturn.BaseType;
+            }
+        }
+    }
+}
