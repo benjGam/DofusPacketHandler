7e0205e6a7fc5e58142f2dcc97745119177130e4
Mecopi <mecopilive@gmail.com>
2023-08-17 08:22:24 +0200

    feat: add class to store recieved message information

---
+++ DofusPacketManager/Network/Messages/NetworkMessageInformations.cs
@@ -0,0 +1,21 @@
+﻿using DofusPacketManager.Utils.IO;
+
+namespace DofusPacketManager.Network.Messages
+{
+    public class NetworkMessageInformations
+    {
+        private ushort _messageId;
+        private int _length;
+        private IDataReader _Reader;
+        public NetworkMessageInformations(ushort messageId, int Length, ref IDataReader Reader) 
+        {
+            _messageId = messageId;
+            _length = Length;
+            _Reader = Reader;
+        }
+
+        public ushort MessageId => _messageId;
+        public int Length => _length;
+        public IDataReader Reader => _Reader;
+    }
+}
