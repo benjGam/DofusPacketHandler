6c7f805478d38a08e5ea6035daae118affa15e09
Mecopi <mecopilive@gmail.com>
2023-08-17 07:29:27 +0200

    feat: add first step of next feat

---
+++ DofusPacketManager/Network/UncompleteNetworkMessage.cs
@@ -0,0 +1,24 @@
+﻿using DofusPacketManager.Networking.Messages;
+using DofusPacketManager.Utils.IO;
+
+namespace DofusPacketManager.Network
+{
+    public class UncompleteNetworkMessage
+    {
+        private BigEndianReader _Reader;
+        private ushort _messageId;
+        private int _Length;
+
+        public UncompleteNetworkMessage(ushort messageId, int length)
+        {
+            _messageId = messageId;
+            _Length = length;
+        }
+
+        #region Accessors
+        public ushort MessageId => _messageId;
+        public int Length => _Length;
+        public IDataReader Reader => _Reader;
+        #endregion
+    }
+}
