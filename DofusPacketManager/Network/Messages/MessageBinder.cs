810d7d604a524ecdcfff157989e6f94ebcca9053
Mecopi <mecopilive@gmail.com>
2023-08-17 11:46:40 +0200

    feat: add base of event binder

--- DofusPacketManager/Network/Messages/MessageBinder.cs
+++ DofusPacketManager/Network/Messages/MessageBinder.cs
@@ -2,34 +2,40 @@
 using DofusPacketManager.Utils;
 using System;
 using System.Collections.Generic;
-using System.Windows.Forms;
+using System.Reflection;
 
 namespace DofusPacketManager.Network.Messages
 {
     public class MessageBinder : Singleton<MessageBinder>
     {
-        private Dictionary<Type, Func<object>> _eventHookerStack = new Dictionary<Type, Func<object>>();
+        private Dictionary<Type, EventHandler> _bindedNetworkMessagesTypes = new Dictionary<Type, EventHandler>();
+        public List<NetworkMessage> _bindedOnDeserialized = new List<NetworkMessage>();
 
         public MessageBinder() 
+        {
+            Init();
+        }
+
+        private void Init()
         {
             PacketParser.Instance.OnMessageRecieved += Instance_OnMessageRecieved;
         }
 
-        public bool On<T>(Func<object> Execute) where T : NetworkMessage
+        public bool Bind<T>(EventHandler handler, string eventName) where T : NetworkMessage
         {
-            if (!_eventHookerStack.ContainsKey(typeof(T)))
+            if (!_bindedNetworkMessagesTypes.ContainsKey(typeof(T)))
             {
-                _eventHookerStack.Add(typeof(T), Execute);
+                _bindedNetworkMessagesTypes.Add(typeof(T), handler);
                 return true;
             }
             return false;
         }
 
-        public bool Unbind<T>() where T : NetworkMessage
+        public bool UnbindDeserialized<T>() where T : NetworkMessage
         {
-            if (_eventHookerStack.ContainsKey(typeof(T)))
+            if (_bindedNetworkMessagesTypes.ContainsKey(typeof(T)))
             {
-                _eventHookerStack.Remove(typeof(T));
+                _bindedNetworkMessagesTypes.Remove(typeof(T));
                 return true;
             }
             return false;
@@ -37,10 +43,13 @@ namespace DofusPacketManager.Network.Messages
 
         private void Instance_OnMessageRecieved(object sender, NetworkMessageReceivedEventArgs e)
         {
-            Type t = e.RecievedMessage.GetType();
-            if (_eventHookerStack.ContainsKey(t))
+            Type targetedType = e.RecievedMessage.GetType();
+            if (_bindedNetworkMessagesTypes.ContainsKey(targetedType))
             {
-                new MethodInvoker(() => _eventHookerStack[t]()).Invoke();
+                EventInfo info = targetedType.GetEvent("OnDeserialized");
+                foreach (NetworkMessage ne in _bindedOnDeserialized)
+                    info.RemoveEventHandler(ne, _bindedNetworkMessagesTypes[targetedType]);
+                info.AddEventHandler(e.RecievedMessage, _bindedNetworkMessagesTypes[targetedType]);
             }
         }
     }
