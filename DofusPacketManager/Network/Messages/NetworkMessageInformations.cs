2e6ec0fa319cfd61e32190892027aca7197b5a5c
Mecopi <mecopilive@gmail.com>
2023-08-20 10:02:25 +0200

    remove useless datareader

--- DofusPacketManager/Network/Messages/NetworkMessageInformations.cs
+++ DofusPacketManager/Network/Messages/NetworkMessageInformations.cs
@@ -6,16 +6,13 @@ namespace DofusPacketManager.Network.Messages
     {
         private ushort _messageId;
         private int _length;
-        private IDataReader _Reader;
         public NetworkMessageInformations(ushort messageId, int Length, ref IDataReader Reader) 
         {
             _messageId = messageId;
             _length = Length;
-            _Reader = Reader;
         }
 
         public ushort MessageId => _messageId;
         public int Length => _length;
-        public IDataReader Reader => _Reader;
     }
 }
