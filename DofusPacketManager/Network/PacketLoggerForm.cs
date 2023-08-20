f27d4db313b8d15055b1ddb0447ac5adeace1183
Mecopi <mecopilive@gmail.com>
2023-08-20 06:01:38 +0200

    refactor & feat: add sub namepace for custom message event args & add base of packet logger

---
+++ DofusPacketManager/Network/PacketLoggerForm.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+
+namespace DofusPacketManager.Network
+{
+    public partial class PacketLoggerForm : Form
+    {
+        private NetworkManager networkManagerInstance;
+        private bool isLogging = false;
+        public PacketLoggerForm()
+        {
+            InitializeComponent();
+            networkManagerInstance = NetworkManager.Instance;
+        }
+
+        private void startLoggingButton_Click(object sender, System.EventArgs e)
+        {
+            if (!networkManagerInstance.Sniffing)
+            {
+                MessageBox.Show("Le programme ne sniffe pas", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //networkManagerInstance.MessageManager.PacketParser.
+        }
+
+        private void packetsLogsTextBox_TextChanged(object sender, System.EventArgs e)
+        {
+            clearLogsButton.Enabled = packetsLogsTextBox.Text.Length > 0;
+            copyLogsButton.Enabled = packetsLogsTextBox.Text.Length > 0;
+        }
+    }
+}
