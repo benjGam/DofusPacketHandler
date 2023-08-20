daa3d5cf873b830f34e1f758c14f808116278cdd
Mecopi <mecopilive@gmail.com>
2023-08-20 04:35:00 +0200

    feat: rename binder func

--- DofusPacketManager/Network/Messages/MessageBinder.cs
+++ DofusPacketManager/Network/Messages/MessageBinder.cs
@@ -11,7 +11,7 @@ namespace DofusPacketManager.Network.Messages
 
         public MessageBinder() => PacketParser.Instance.MessageReceived += PacketParser_OnMessageRecieved;
 
-        public void Bind<T>(EventHandler handler, NetworkMessageEventEnum eventName) where T : NetworkMessage
+        public void On<T>(EventHandler handler, NetworkMessageEventEnum eventName) where T : NetworkMessage
         {
             if (!_bindedNetworkMessagesTypes.ContainsKey(typeof(T)))
                 _bindedNetworkMessagesTypes.Add(typeof(T), new EventKeyPair(eventName.ToString(), handler));
